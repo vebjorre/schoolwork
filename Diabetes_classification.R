fca1d389c74142662f554287b2472b1d15d184f6
vebjorre <vebjorre@stud.ntnu.no>
2019-10-17 16:44:41 +0200

    clean-up of Diabetes_classification.R

--- Diabetes_classification.R
+++ Diabetes_classification.R
@@ -1,149 +1,205 @@
 #Small project done in TMA4268 Statistical learning
 #Using different statistical methods to determine whether women have diabetes or not
 
-#Importing data set
-flying=dget("https://www.math.ntnu.no/emner/TMA4268/2019v/data/flying.dd") 
-ctrain=flying$ctrain
-ctest=flying$ctest
-# transform(ctrain, diabetes = as.factor(diabetes))
-# transform(ctest, diabetes = as.factor(diabetes))
-
-#Summary of training data:
-summary(ctrain)
-
+#Load packages used in project:
 library(tidyverse)
 library(caret)
 library(pROC)
+library(ggplot2)
+library(GGally)
+library(corrplot)
+library(MASS)
+library(e1071)
+library(tree)
+library(nnet)
+library(NeuralNetTools)
+
+#Import data set
+flying=dget("https://www.math.ntnu.no/emner/TMA4268/2019v/data/flying.dd")
+ctrain=flying$ctrain
+ctest=flying$ctest
+#Summary of training data:
+summary(ctrain)
+
+
+#ctrain.tmp<-ctrain
+#ctrain.tmp$diabetes<-as.factor(ctrain.tmp$diabetes)
+#ctrain.tmp$npreg<-as.factor(ctrain.tmp$npreg)
+#ggpairs(ctrain.tmp,cardinality_threshold = 16)
+corrplot(cor(ctrain))
+# ggpairs(ctrain)
+
+#Scale and center data
+mean <- apply(ctrain[,-1], 2, mean) #not diabetes, in column 1                                 
+std <- apply(ctrain[,-1], 2, sd)
+train.x <- data.frame(scale(ctrain[,-1], center = mean, scale = std))
+test.x <- data.frame(scale(ctest[,-1], center = mean, scale = std)) # normalized based on the whole training set
+
+train.y <- as.factor(ctrain[,1])
+test.y <- as.factor(ctest[,1])
+train.xy <- data.frame(train.x,diabetes=train.y)
+test.xy <- data.frame(test.x,diabetes=test.y)
+
+resmat=matrix(ncol=2,nrow=8)
+# misclassification rate test data, AUC
+# for all the methods considered here
+colnames(resmat)=c("Misclassification rate","AUC")
+rownames(resmat)=c("KNN","LDA","logistic regression","decision tree","pruned tree","SVC","SVM","NN h0")
 
 #===============================================================================================
+#Need factors to have valid var.names for trainControl()
+knn.train.y <- as.factor(ctrain[,1]) 
+knn.test.y <- as.factor(ctest[,1])
+knn.train.y <- factor(knn.train.y, labels=c("O","I")) 
+knn.test.y <- factor(knn.test.y, labels=c("O", "I"))
+knn.train.xy <- data.frame(train.x,diabetes=knn.train.y)
+knn.test.xy <- data.frame(test.x,diabetes=knn.test.y)
 
 #K nearest neighbours classification:
 
 set.seed(0)
 #CV to find best K
-train.control = trainControl(method = "cv", number=5)
-model = train(ctrain[,-1], as.factor(ctrain[,1]), "knn", trControl = train.control)
-# model = train(diabetes ~., data=ctrain, method="knn", trControl=train.control)
-print(model)
-#Training model with K=5
-KNNclass = class::knn(train=ctrain, cl=ctrain$diabetes, test=ctest, k=5, prob=TRUE)
+train.control <- trainControl(method = "cv", number=5, classProbs=TRUE, summaryFunction = twoClassSummary)
+#train knn model
+knn.mod <- train(diabetes ~ ., data=knn.train.xy, "knn", trControl = train.control,tuneLength=20)
+print(knn.mod)
+#test with test data
+knn.res <- predict(knn.mod,newdata=test.x)
+knn.prob <- predict(knn.mod,newdata=test.x, type="prob")
+knn.conf <- confusionMatrix(knn.res,knn.test.y)$table
+knn.missrate <- 1 - sum(diag(knn.conf))/sum(knn.conf)
+#plot ROC
+knn.roc <- roc(knn.test.y, knn.prob[,2])
+ggroc(knn.roc)+ggtitle("ROC curve - K-Nearest-Neighbours") + annotate("text", x = 0.25, y = 0.3, label = paste("AUC = ", as.character(round(auc(knn.roc), 4))))
 
-KNNprobdiabetes = attributes(KNNclass)$prob
-KNNprob = ifelse(KNNclass == "0", 1-KNNprobdiabetes, KNNprobdiabetes)
+resmat[1,1] = knn.missrate
+resmat[1,2] = knn.roc$auc
+#===============================================================================================
 
-misclass = table(KNNclass, ctest$diabetes)
-#misclassification rate:
-1 - sum(diag(misclass))/sum(misclass)
+#train lda model
+lda.mod <- lda(diabetes~npreg+glu+bp+skin+bmi+ped+age, data=train.xy,prior=c(0.5,0.5))
+lda.res <- predict(lda.mod, newdata = test.x)
+lda.conf <- confusionMatrix(lda.res$class,test.y)$table
+lda.missrate <- 1 - sum(diag(lda.conf))/sum(lda.conf)
 
-KNN.roc = roc(ctest$diabetes, KNNprob)
-KNN.auc = auc(ctest$diabetes, KNNprob)
+lda.roc = roc(test.y,lda.res$posterior[,2],legacy.axes=TRUE,auc=TRUE)
+ggroc(lda.roc)+ggtitle("ROC curve - Linear discriminant analysis") + annotate("text", x = 0.25, y = 0.3, label = paste("AUC = ", as.character(round(auc(lda.roc), 4))))
 
-ggroc(KNN.roc) + ggtitle("ROC curve") + annotate("text", x = 0.25, y = 0.3, 
-            label = paste("AUC = ", as.character(round(KNN.auc, 4))))
+resmat[2,1] = lda.missrate
+resmat[2,2] = lda.roc$auc
+#===============================================================================================
 
+#train logistic regression model
+logist.mod=glm(diabetes~., data=train.xy,family="binomial")
+summary(logist.mod)
+logist.prob <- predict(logist.mod, newdata = test.x,type="response")
+logist.res <- factor(ifelse(logist.prob>=0.5,1,0))
+logist.conf <- confusionMatrix(test.y,logist.res)$table
+logist.missrate <- 1 - sum(diag(logist.conf))/sum(logist.conf)
+
+# logist.roc = roc(test.y,logist.res,legacy.axes=TRUE)
+logist.roc <- roc(test.y,logist.prob)
+ggroc(logist.roc)+ggtitle("ROC curve - Logistic regression") + annotate("text", x = 0.25, y = 0.3, label = paste("AUC = ", as.character(round(auc(logist.roc), 4))))
+
+resmat[3,1] = logist.missrate
+resmat[3,2] = logist.roc$auc
 #===============================================================================================
 
 #Decision tree:
-library(tree)
+
 set.seed(2)
-tree.dia = tree(formula = as.factor(diabetes) ~ ., ctrain, split="gini")
+tree.mod <- tree(diabetes ~ ., train.xy, split="gini")
 
-plot(tree.dia)
-text(tree.dia, pretty=1, cex=.8)
+plot(tree.mod)
+text(tree.mod, pretty=1, cex=.8)
 
-summary(tree.dia)
+summary(tree.mod)
 
-yhat.tree = predict(tree.dia, ctest, type = "class")
-response.test = ctest$diabetes
-misclass = table(yhat.tree, response.test)
-print(misclass)
+tree.res <- predict(tree.mod, newdata = test.x, type="class")
+tree.conf <- confusionMatrix(tree.res, test.y)$table
+print(tree.conf)
 #Misclassification rate:
-1 - sum(diag(misclass))/sum(misclass)
+tree.missrate <- 1 - sum(diag(tree.conf))/sum(tree.conf)
 
-#CV to find best number of splits in tree
-cv.dia = cv.tree(tree.dia, FUN = prune.misclass)
-plot(cv.dia$size, cv.dia$dev, type = "b")
-
-#Prune tree down to 10 splits
-prune.dia = prune.misclass(tree.dia, best = 10)
-summary(prune.dia)
-plot(prune.dia)
-text(prune.dia, pretty = 1, cex=0.8)
-
-yhat.prune = predict(prune.dia, ctest, type="class")
-prob.prune = prune.dia$yprob
-misclass.prune = table(yhat.prune, response.test)
-print(misclass.prune)
-#Misclassification rate:
-1 - sum(diag(misclass.prune))/sum(misclass.prune)
-
-prob.prune = predict(prune.dia, ctest) 
-prune.roc = roc(ctest$diabetes, prob.prune[,1], auc=TRUE) 
-prune.auc = prune.roc$auc
-
-ggroc(prune.roc) + ggtitle("ROC curve") + annotate("text", x = 0.25, y = 0.3, 
-                                                   label = paste("AUC = ", as.character(round(prune.auc, 4))))
-
+resmat[4,1] = tree.missrate
 #===============================================================================================
 
-#Support vector machine:
-#NB! Should have scaled the data here.
-library(e1071)
+#CV to find best number of splits in tree
+#Not completely correct to use cv on already scaled data
+tree.cv <- cv.tree(tree.mod, FUN = prune.misclass)
+plot(tree.cv$size, tree.cv$dev, type = "b")
 
-svmfit_linear = svm(diabetes ~ ., data = ctrain, kernel = "linear", cost = 1, 
-                    scale = FALSE, type="C-classification")
-summary(svmfit_linear)
+#Prune tree down to 10 splits
+pruned.mod <- prune.tree(tree.mod, best = 10)
+summary(pruned.mod)
+plot(pruned.mod)
+text(pruned.mod, pretty = 1, cex=0.8)
+
+pruned.res <- predict(pruned.mod, newdata=test.x, type="class")
+pruned.conf <- confusionMatrix(pruned.res, test.y)$table
+
+#Misclassification rate:
+pruned.missrate <- 1 - sum(diag(pruned.conf))/sum(pruned.conf)
+
+resmat[5,1] = pruned.missrate
+#===============================================================================================
+
+#Support vector classifier:
 
 set.seed(1)
-cv_linear = tune(svm, as.factor(diabetes) ~., data=ctrain, 
+svc.cv <- tune(svm, diabetes ~., data=train.xy, 
                  kernel="linear", type="C-classification",
-                 ranges=list(cost=c(0.001,0.01,0.1,1,5,10,100)))
-summary(cv_linear)
-bestmod_linear = cv_linear$best.model
+                 ranges=list(cost=c(0.001,0.01,0.1,1,5,10,100), gamma=c(0.01,1,5,10)))
+svc.mod <- svc.cv$best.model
 
-yhat.linear = predict(bestmod_linear, ctest, type="class")
-table(predict = yhat.linear, truth=ctest$diabetes)
+svc.res <- predict(svc.mod, test.x, type="class")
+svc.conf <- confusionMatrix(svc.res, test.y)$table
+svc.missrate <- 1 - sum(diag(svc.conf))/sum(svc.conf)
 
-set.seed(1)
-cv_radial = tune(svm, as.factor(diabetes) ~ npreg + glu + bmi + ped, data=ctrain, 
+svm.cv <- tune(svm, as.factor(diabetes) ~ ., data=train.xy, 
                  kernel="radial", type="C-classification",
-                 ranges=list(cost=c(0.001,0.01,0.1,1,5,10,100)))
-summary(cv_radial)
-bestmod_radial = cv_radial$best.model
+                 ranges=list(cost=c(0.001,0.01,0.1,1,5,10,100),gamma=c(0.01,1,5,10)))
+summary(svm.cv)
+svm.mod <- svm.cv$best.model
 
-yhat.radial = predict(bestmod_radial, ctest, type="class")
-table(predict = yhat.radial, truth=ctest$diabetes)
+svm.res <- predict(svm.mod, test.x, type="class")
+svm.conf <- confusionMatrix(svm.res,test.y)$table
+svm.missrate <- 1 - sum(diag(svm.conf))/sum(svm.conf)
+
+resmat[6,1] = svc.missrate
+resmat[7,1] = svm.missrate
 
 #===============================================================================================
 
 #Feed forward neural network with one hidden layer
-#(Not a deliberate decision)
+#No hidden layers are better in this situation -> logistic regression
+#nnet allows only zero or one hidden layer
 
-library(nnet)
-library(NeuralNetTools)
+#Unscaled data:
+train.data <- ctrain[,-1]
+train.target <- as.factor(ctrain[,1])
+test.data <- ctest[,-1]
+test.target <- as.factor(ctest[,1])
+
+#CV to decide number of nodes in hidden layer:
+
 set.seed(0)
-
-train.data = ctrain[,-1]
-train.target = as.factor(ctrain[,1])
-test.data = ctest[,-1]
-test.target = as.factor(ctest[,1])
-
-#CV to decide number of nodes in hidden layer (less than 5 could be better):
-grid = c(5,10,15,20,25,30,50)
-k = 5
-indices = sample(1:nrow(train.data))
-folds = cut(indices, breaks = k, labels=FALSE)
-mismat = matrix(NA, ncol=k, nrow=length(grid))
+num.nodes <- c(0:10,15,20,25,30,50)
+# num.nodes <- c(0:20)
+k <- 5
+indices <- sample(1:nrow(train.data))
+folds <- cut(indices, breaks = k, labels=FALSE)
+mismat <- matrix(NA, ncol=k, nrow=length(num.nodes))
 for (j in 1:k){
-  thistrain = (1:dim(train.data)[1])[folds != j]
-  thisvalid = (1:dim(train.data)[1])[folds == j]
-  mean = apply(train.data[thistrain, ], 2, mean)
-  std = apply(train.data[thistrain, ], 2, sd)
+  thistrain <- (1:dim(train.data)[1])[folds != j]
+  thisvalid <- (1:dim(train.data)[1])[folds == j]
+  mean <- apply(train.data[thistrain, ], 2, mean)
+  std <- apply(train.data[thistrain, ], 2, sd)
   new = scale(train.data, center=mean, scale=std)
-  for (i in 1:length(grid)) {
-    thissize = grid[i]
+  for (i in 1:length(num.nodes)) {
+    thissize = num.nodes[i]
     
-    fit = nnet(train.target ~., data=new, size=thissize, maxit=5000)
+    fit = nnet(formula=train.target ~ npreg+glu+bp+skin+bmi+ped+age, data=new, size=thissize, maxit=100, skip=TRUE)
     pred = predict(fit, newdata=new[thisvalid, ], type="class")
     misclass = table(pred, new[thisvalid, 1])
     miss_rate = sum(diag(misclass))/sum(misclass)
@@ -152,22 +208,21 @@ for (j in 1:k){
 }
 
 
-average.misclass = apply(mismat, 1, sum)/k
+average.misclass <- apply(mismat, 1, sum)/k
 plot.window(xlim=c(0,50),ylim=c(0,100))
-plot(grid,average.misclass, type="l")
+plot(num.nodes,average.misclass, type="l")
 
-set.seed(0)
-mean = apply(train.data, 2, mean)
-std = apply(train.data, 2, sd)
-train.new = scale(train.data, center=mean, scale=std)
-fit5 = nnet(ctrain[,1] ~., data=train.new, size=5, maxit=5000, linout=FALSE, entropy=TRUE)
-plotnet(fit5)
+nnet.mod <- nnet(formula=diabetes~npreg+glu+bp+skin+bmi+ped+age, data=train.xy, size=0, linout=FALSE, skip=TRUE, maxit=5000)
 
-test.new = scale(test.data, center=mean, scale=std)
+nnet.res <- predict(nnet.mod, newdata=test.x, type="class")
+nnet.conf <- confusionMatrix(factor(nnet.res), test.y)$table
+nnet.missrate = 1 - sum(diag(nnet.conf))/sum(nnet.conf)
 
-pred = predict(fit5, newdata=test.new, type="class")
-misclass.5 = table(pred, ctest[,1])
+nnet.prob <- predict(nnet.mod, newdata=test.x)
+nnet.roc <- roc(test.y, as.numeric(nnet.prob), legacy.axes=TRUE)
+ggroc(nnet.roc)+ggtitle("ROC curve - Neural network") + annotate("text", x = 0.25, y = 0.3, label = paste("AUC = ", as.character(round(auc(nnet.roc), 4))))
 
-misclass.5
-miss_rate = 1 - sum(diag(misclass.5))/sum(misclass.5)
-miss_rate
+resmat[8,1] = nnet.missrate
+resmat[8,2] = nnet.roc$auc
+
+print(resmat)
